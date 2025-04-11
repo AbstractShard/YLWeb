98bdaff276794286d52400d50ea0ff0c06fa8a0f
AbstractShard <165806563+AbstractShard@users.noreply.github.com>
2025-04-11 12:30:07 +0300

    small changes

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-# TODO: class ProfileForm, func show_profile
-
 from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 
