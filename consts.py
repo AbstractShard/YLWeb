5dd81e564d515fe51f875201553ae41014bb603a
AbstractShard <165806563+AbstractShard@users.noreply.github.com>
2025-04-13 12:18:26 +0300

    small changes

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 from functools import wraps
 import os
 
-from flask_login import current_user
+from flask_login import current_user, AnonymousUserMixin
 
 DEFAULT_PROFILE_PATH = "static/img/profile.png"
 CURRENT_PROFILE_PATH = "static/buffer/profile.png"
@@ -10,10 +10,15 @@ CURRENT_PROFILE_PATH = "static/buffer/profile.png"
 def check_buffer(func):
     @wraps(func)
     def body(*args, **kwargs):
+        # Проверка на наличие папки "buffer"
+        if not os.path.exists("static/buffer"):
+            os.mkdir("static/buffer")
+
         # Проверка на наличие файла с изображением профиля
         if not os.path.exists(CURRENT_PROFILE_PATH):
-            with open(CURRENT_PROFILE_PATH, mode="wb") as curr_img:
-                curr_img.write(current_user.img)
+            if not isinstance(current_user, AnonymousUserMixin):
+                with open(CURRENT_PROFILE_PATH, mode="wb") as curr_img:
+                    curr_img.write(current_user.img)
 
         return func(*args, **kwargs)
 
